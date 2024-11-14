027bf11f4c463a9c63bdfcebc38b235963ba9301
Narek Germirlian <ngermirlian@gmail.com>
2024-11-14 08:43:54 -0800

    Added function clean_all_articles in articleCleaner.py to clean all articles instead of just one

--- articleCleaner.py
+++ articleCleaner.py
@@ -13,3 +13,18 @@ def clean_article(text):
     text = re.sub(r'\s+', ' ', text).strip()
     
     return text
+
+
+def clean_all_articles(articles):
+    """
+    For now, implementation requires that articles is in the form [{"id": "...", "text": "..."}, {...}]
+    FIX: This will change once we use wordpress' endpoint
+    """
+    cleaned_articles = []
+    # Loop through articles
+    for article in articles:
+        # Clean article, append to cleaned articles
+        cleaned_articles.append({"id": article["id"], "text": clean_article(article["text"])})
+
+    return cleaned_articles
+    
